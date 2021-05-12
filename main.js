be92335284d7e7ed238ba62941c528c8ef502bcd
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-05-13 00:45:33 +0300

    practice Q1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
-
+const authRouter = express.Router();
 const users = ["John", "Mark"];
 app.use(express.json());
 
@@ -28,10 +28,10 @@ app.get("/users", logMethod, (req, res, next) => {
     res.json(users);
 });
 
-app.get("/users", (req, res, next) => {
+authRouter.use("./users",(req, res, next) => {
     res.json(users);
     next();
-});
+  });
 
 app.use((err, req, res, next) => {
   // set the status code
